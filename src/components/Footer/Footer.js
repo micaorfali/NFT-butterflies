8cac23e786d879f90e484c28a2d4fc2082c2d31d
micaorfali <mica.orfali@gmail.com>
2022-11-17 20:02:18 -0300

    probando

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
-      <h2 >Butterfly World</h2>
+      <h2 >Butterfly World HOLA</h2>
       <p className={styles.copyright}>© 2022 Butterfly World. Design with by Micaela Orfali.</p>
     </div>
   );
